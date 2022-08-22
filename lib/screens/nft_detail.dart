6271d2f08a5cdd7a00b1c701bebb2979927e845c
Charawey-X <charawey@gmail.com>
2022-08-22 22:20:28 +0300

    complete nft-detail screen

--- lib/screens/nft_detail.dart
+++ lib/screens/nft_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nft/utils/color_util.dart' as color_util;
 
 class NftDetail extends StatefulWidget {
@@ -9,6 +10,19 @@ class NftDetail extends StatefulWidget {
 }
 
 class _NftDetailState extends State<NftDetail> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +35,7 @@ class _NftDetailState extends State<NftDetail> {
           children: [
             Expanded(
                 child: Padding(
-              padding: const EdgeInsets.only(top: 100),
+              padding: const EdgeInsets.only(top: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -38,7 +52,7 @@ class _NftDetailState extends State<NftDetail> {
                         ),
                       ],
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(100),
+                      borderRadius: BorderRadius.circular(50),
                     ),
                     child: IconButton(
                       alignment: Alignment.center,
@@ -89,7 +103,176 @@ class _NftDetailState extends State<NftDetail> {
                   )
                 ],
               ),
-            ))
+            )),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1660840051745-2c0da0466381?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'),
+                  fit: BoxFit.cover,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 5,
+                    spreadRadius: 5,
+                  ),
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            Expanded(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const SizedBox(width: 20),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.1,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+                      fit: BoxFit.cover,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.6),
+                        blurRadius: 5,
+                        spreadRadius: 5,
+                      ),
+                    ],
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                      Text("Creator",
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          )),
+                      Text("Saido Mana",
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ))
+                    ])),
+                const SizedBox(
+                  width: 35,
+                ),
+                const Text("EmasSilver #03",
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                    )),
+                const SizedBox(width: 20),
+              ],
+            )),
+            const Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                )),
+            const SizedBox(height: 20),
+            Expanded(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const SizedBox(width: 20),
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("  Last Bid",
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                        )),
+                    const SizedBox(height: 10),
+                    RichText(
+                        text: const TextSpan(
+                      style: TextStyle(
+                        color: Colors.brown,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      children: [
+                        WidgetSpan(
+                            child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Icon(Icons.anchor_rounded,
+                              color: Colors.brown, size: 20),
+                        )),
+                        TextSpan(text: "7.19 ETH"),
+                      ],
+                    )),
+                  ],
+                )),
+                RichText(
+                    text: const TextSpan(
+                  style: TextStyle(
+                    height: 1.5,
+                    color: Colors.brown,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  children: [
+                    WidgetSpan(
+                        child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Icon(Icons.timer, color: Colors.brown, size: 30),
+                    )),
+                    TextSpan(text: "01h : 24m : 57s")
+                  ],
+                )),
+                const SizedBox(width: 20),
+              ],
+            )),
+            const SizedBox(height: 20),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.07,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 5,
+                    spreadRadius: 5,
+                  ),
+                ],
+                color: color_util.ColorUtil.fromHex("#FBE4D7"),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: const Text(
+                "PLACE A BID",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
           ],
         ),
       ),
